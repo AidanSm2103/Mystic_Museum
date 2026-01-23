42845dd7da65e555325d3f83700c10a53a4575ce
Aidan Smith <aidansm2103@gmail.com>
2026-01-23 15:51:28 +0200

    Added on click events for the runes to change the image of their corresponding artifacts.

---
+++ JavaScript/Script.js
@@ -0,0 +1,41 @@
+
+const rune1 = document.getElementById('reliquary-rune');
+const artifact1 = document.getElementById('reliquary-artifact');
+
+const rune2 = document.getElementById('astrolabe-rune');
+const artifact2 = document.getElementById('astrolabe-artifact');
+
+const rune3 = document.getElementById('sigil-rune');
+const artifact3 = document.getElementById('sigil-artifact');
+if (rune1 && artifact1) 
+    {
+        rune1.addEventListener('click', () => {
+        const currentSrc = artifact1.src;
+        const altSrc = artifact1.getAttribute('data-alt');
+
+        artifact1.src = altSrc;
+        artifact1.setAttribute('data-alt', currentSrc);
+    });
+    }
+
+if (rune2 && artifact2)
+    {   
+  rune2.addEventListener('click', () => {
+    const currentSrc = artifact2.src;
+    const altSrc = artifact2.getAttribute('data-alt');
+
+    artifact2.src = altSrc;
+    artifact2.setAttribute('data-alt', currentSrc);
+  });
+}
+
+if (rune3 && artifact3)
+    {
+  rune3.addEventListener('click', () => {
+    const currentSrc = artifact3.src;
+    const altSrc = artifact3.getAttribute('data-alt');
+
+    artifact3.src = altSrc;
+    artifact3.setAttribute('data-alt', currentSrc);
+  });
+    }
