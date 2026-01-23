edb3c32ba64433a0354b9c96dee0550a4925de44
Aidan Smith <aidansm2103@gmail.com>
2026-01-23 16:09:59 +0200

    Stored clicks in local storage to fix the images not saving when moving to a different page.

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -7,35 +7,51 @@ const artifact2 = document.getElementById('astrolabe-artifact');
 
 const rune3 = document.getElementById('sigil-rune');
 const artifact3 = document.getElementById('sigil-artifact');
-if (rune1 && artifact1) 
+
+if (rune1)
     {
         rune1.addEventListener('click', () => {
+        localStorage.setItem('reliquaryUnlocked', 'true');
+    });
+    }
+
+    if (artifact1 && localStorage.getItem('reliquaryUnlocked') === 'true')
+        {
         const currentSrc = artifact1.src;
         const altSrc = artifact1.getAttribute('data-alt');
 
         artifact1.src = altSrc;
         artifact1.setAttribute('data-alt', currentSrc);
-    });
-    }
+        }
 
-if (rune2 && artifact2)
+if (rune2)
     {   
   rune2.addEventListener('click', () => {
+    localStorage.setItem('astrolabeUnlocked', 'true');
+  });
+}
+
+if (artifact2 && localStorage.getItem('astrolabeUnlocked') === 'true')
+    {
     const currentSrc = artifact2.src;
     const altSrc = artifact2.getAttribute('data-alt');
 
     artifact2.src = altSrc;
     artifact2.setAttribute('data-alt', currentSrc);
-  });
-}
+    }
 
-if (rune3 && artifact3)
+if (rune3)
     {
   rune3.addEventListener('click', () => {
-    const currentSrc = artifact3.src;
-    const altSrc = artifact3.getAttribute('data-alt');
-
-    artifact3.src = altSrc;
-    artifact3.setAttribute('data-alt', currentSrc);
+    localStorage.setItem('sigilUnlocked', 'true');
   });
     }
+
+     if (artifact3 && localStorage.getItem('sigilUnlocked') === 'true') 
+        {
+            const currentSrc = artifact3.src;
+            const altSrc = artifact3.getAttribute('data-alt');
+
+            artifact3.src = altSrc;
+            artifact3.setAttribute('data-alt', currentSrc);
+        }
