3cc9ca54b0c2f029bf8119daefb96c122cdb4ae8
Aidan Smith <aidansm2103@gmail.com>
2026-02-02 20:12:43 +0200

    Created an updateArtifactsImages function which allows artifact images to be updated without reloading the page which was a problem on the Exhibits page rune before.

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -4,7 +4,6 @@ localStorage.setItem("reliquary-unlocked", "false");
 localStorage.setItem("astrolabe-unlocked", "false");
 localStorage.setItem("sigil-unlocked", "false");
 */
-
 const revealBtn = document.getElementById('reveal-btn');
 const hiddenLore = document.querySelector('.hidden-lore');
 
@@ -15,19 +14,25 @@ if (revealBtn)
     });
   }
 
+function updateArtifactImages() 
+{
+  document.querySelectorAll('[id$="-artifact"]').forEach(img => {
+    const key = img.id.replace('-artifact', '');
+
+    if (localStorage.getItem(`${key}-unlocked`) === 'true') {
+      img.src = img.dataset.unlocked;
+  }
+});
+}
+
+updateArtifactImages();
+
 document.querySelectorAll('.interactive-rune').forEach(rune => {
   rune.addEventListener('click', () => {
     const key = rune.dataset.artifact;
 
     localStorage.setItem(`${key}-unlocked`, 'true');
+    updateArtifactImages();
   });
 });   
-
-document.querySelectorAll('[id$="-artifact"]').forEach(img => {
-  const key = img.id.replace('-artifact', '');
-
-  if (localStorage.getItem(`${key}-unlocked`) === 'true') {
-    img.src = img.dataset.unlocked;
-  }
-});
 
